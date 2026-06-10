b3d4b27e8b0316b73ea2747db99327f31eacecb7
keerthieshvar-droid <keerthieshvar@gmail.com>
2026-06-10 16:24:41 +0530

    Update and rename code.txt to sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+           int a =10;
+           float b =12.5;
+           char c = 'X';
+
+           printf("Size of a = %lu bytes\n" , sizeof(a));
+           printf("Size of b = %lu bytes\n" , sizeof(b));
+           printf("Size of c = %lu bytes\n" , sizeof(c));
+
+        return 0;
+}

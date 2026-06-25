cfca6655a6a5bf39fd3a217085eab1363e45eb18
keerthieshvar-droid <keerthieshvar@gmail.com>
2026-06-25 18:08:32 +0530

    Add files via upload

---
+++ simcal.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+
+            float num1,num2;
+            char op;
+
+            printf("Enter first number:");
+            scanf("%f" , &num1);
+
+            printf("Enter operator ( +, -, *, /):\n");
+            scanf(" %c", &op);
+
+            printf("Enter second number:");
+            scanf("%f", &num2);
+
+            switch(op){
+                   case '+':
+                     printf("Result = %.2f", num1 + num2);
+                     break;
+
+                   case '-':
+                     printf("Result = %.2f", num1 - num2);
+                     break;
+
+                   case '*':
+                     printf("Result = %.2f", num1 * num2);
+                     break;
+
+                   case '/':
+                      if(num2 != 0)
+                            printf("Result = %.2f", num1 / num2);
+                      else
+                            printf("Division by zero is not allowed.");
+                      break;
+
+                   default:
+                            printf("Invalid operator.");
+       }
+            return 0;
+}

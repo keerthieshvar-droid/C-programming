485d514aec6ad6b7f017c6c5860a32e8dbaa4387
keerthieshvar-droid <keerthieshvar@gmail.com>
2026-06-26 09:52:19 +0530

    Add files via upload

---
+++ oe.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+             int num;
+
+            printf("Enter a number: \n");
+            scanf("%d" , &num);
+
+           if(num %2 == 0)
+              {
+              printf("%d is a even number.\n" , num);
+              }
+           else
+              {
+               printf("%d is a odd number.\n" , num);
+              }
+
+       return 0;
+}

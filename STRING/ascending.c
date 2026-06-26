a85df9e0e0106307fdcdc8deb752e45e492dab6e
keerthieshvar-droid <keerthieshvar@gmail.com>
2026-06-26 12:54:19 +0530

    Add files via upload

---
+++ STRING/ascending.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+            int arr[] = {25,15,30,5,10};
+            int n = sizeof(arr) / sizeof(arr[0]);
+            int i,j,temp;
+
+            for(i = 0; i < n-1; i++){
+              for(j = 0; j < n-1; j++){
+                if(arr[j] > arr[j+1]){
+                 temp = arr[j];
+                 arr[j] = arr[j+1];
+                 arr[j+1] = temp;
+               }
+             }
+           }
+
+               printf("Array in ascending order:\n");
+                for(i = 0; i < n; i++){
+                  printf("%d",arr[i]);
+          }
+        return 0;
+}

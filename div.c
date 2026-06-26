3ffa4579ada348df4dfa40ec4c63b2a70c5affcb
keerthieshvar-droid <keerthieshvar@gmail.com>
2026-06-26 09:33:07 +0530

    Add files via upload

---
+++ div.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+
+             int regno = 42130225;
+             char grade = 'A';
+             float mark = 88.21;
+             double percentage =  88.21;
+
+
+             printf("Reg No = %d\n" , regno);
+             printf("Grade = %c\n" , grade);
+             printf("Mark = %.2f\n" , mark);
+             printf("Percentage = %.2lf\n" , percentage);
+
+        return 0;
+}

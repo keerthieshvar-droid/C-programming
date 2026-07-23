0a0a076a382df907b50ca4558fc0fa9443f98a08
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-07-23 11:26:51 +0530

    Add files via upload

---
+++ C_practice/arithmetic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10,b = 20;
+
+     printf("Sum : %d\n" , a + b);
+
+     printf("Diff : %d\n" , a - b);
+
+     printf("Prod : %d\n" , a * b);
+
+     printf("Quot : %d\n" , a / b);
+
+return 0;
+}
---
+++ C_practice/variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+    double d = 3.14159;
+
+   printf("a : %d\n" , a);
+
+   printf("b : %.2f\n" , b);
+
+   printf("c : %c\n" , c);
+
+   printf("d : %lf\n", d);
+
+
+return 0;
+}

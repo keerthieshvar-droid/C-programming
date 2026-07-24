f7a4f9824ed366031f0fa37491bd669f1ff72bb9
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-07-24 14:56:51 +0530

    Add files via upload

---
+++ controlstructure/oddeven.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int num;
+   printf("Enter a number :");
+   scanf("%d" , &num);
+
+   if(num % 2 == 0)
+{
+     printf("Even : %d\n" , num);
+}
+   else
+{
+    printf("Odd : %d\n" , num);
+}
+
+  return 0;
+}

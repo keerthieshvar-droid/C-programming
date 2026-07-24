64d49da847292a85f752a762315c6af6e6cf0bd1
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-07-24 09:40:01 +0530

    Add files via upload

---
+++ C_practice/incrrement.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+
+    printf("Initial a = %d\n\n", a);
+
+     printf("Output of a++ : %d\n", a++);
+     printf("Value of a now : %d\n",a);
+
+     printf("Output of ++a : %d\n" , ++a);
+     printf("Value of a now : %d\n" , a);
+
+     printf("Output of a-- : %d\n" , a--);
+     printf("Value of a now :%d\n" , a);
+
+     printf("Output of --a : %d\n" , --a);
+     printf("Value of a now : %d\n" , a);
+
+return 0 ;
+}

bb0834d977fc00e9d967483e86655a30ede6473c
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-08-10 11:25:08 +0530

    Add files via upload

---
+++ Function/fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void fibonacci(int n)
+{
+         int a = 0, b = 1, c,i;
+
+         for(i = 0; i < n; i++)
+{
+         printf("%d" , a);
+
+         c = a + b;
+         a = b;
+         b = c;
+}
+}
+
+int main()
+{
+     int n;
+
+     printf("Enter a number: ");
+     scanf("%d" , &n);
+
+    fibonacci(n);
+
+   return 0;
+}

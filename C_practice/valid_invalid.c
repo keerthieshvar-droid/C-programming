fbd3f305ea6d6c78e8f9b5228c081b9c2730dd11
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-08-12 15:20:38 +0530

    Add files via upload

---
+++ C_practice/valid_invalid.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int sequences(int arr[],int n)
+{
+     for(int i = 0; i < n; i++)
+        {
+          if(arr[i] - arr[i + 1] == 1)
+           {
+              return 1;
+           }
+          else
+           {
+              return 0;
+           }
+       }
+}
+int main()
+{
+   int arr[25],n,i;
+  printf("Enter a number of elements:");
+   scanf("%d" , &n);
+
+  printf("Enter array elements:");
+   for(i = 0; i < n; i++)
+      {
+        scanf("%d" , &arr[i]);
+      }
+
+   if(sequences(arr, n))
+     {
+       printf("Valid sequences");
+     }
+   else
+     {
+      printf("Invalid sequences");
+     }
+
+ return 0;
+}

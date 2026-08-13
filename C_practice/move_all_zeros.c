3f00128ee5f777ee93d3188e6e9d9c7fb5e46305
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-08-13 09:17:52 +0530

    Add files via upload

---
+++ C_practice/move_all_zeros.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+   int arr[100],n,i,j = 0;
+   printf("Enter a number of elements:");
+   scanf("%d" , &n);
+
+   printf("Enter array elements:");
+    for(i = 0; i < n; i++)
+     {
+        scanf("%d" , &arr[i]);
+     }
+
+    for(i = 0; i < n; i++)
+     {
+       if(arr[i] != 0)
+        {
+          arr[j] = arr[i];
+           j++;
+        }
+     }
+
+while(j < n)
+  {
+       arr[j] = 0;
+         j++;
+   }
+
+printf("Array after moving zeros to end :\n");
+for(i = 0; i < n; i++)
+{
+   printf("%d" , arr[i]);
+}
+
+  return 0;
+}

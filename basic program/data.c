05750be48e7f0e1621e9833921042c516731c30f
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-08-18 10:03:00 +0530

    Add files via upload

---
+++ basic program/data.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+
+       int age  = 25;
+       float height = 6.2;
+       char grade = 'A';
+       double salary = 30000;
+
+       printf("Age = %d\n", age);
+       printf("Height = %.1f\n", height);
+       printf("Grade = %c\n", grade);
+       printf("Salary = %.2lf\n", salary);
+
+return 0;
+}

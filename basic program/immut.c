237ee12ab5a7399815122937d2effd408f3bdce8
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-08-24 12:43:46 +0530

    Add files via upload

---
+++ basic program/immut.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+
+              const int num = 10;
+              printf("Value of num = %d\n" , num);
+
+              printf("Constant values remains unchanged = %d\n" ,  num);
+
+      return 0;
+}

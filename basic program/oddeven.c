f6a9e98b03e9bf4b57b0a037eaaa4827ffe361d3
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-08-27 09:58:07 +0530

    Add files via upload

---
+++ basic program/oddeven.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+   int num;
+
+   printf("Enter a number :");
+   scanf("%d" , &num);
+
+   if(num % 2 == 0)
+{
+    printf("Even : %d\n");
+}
+else{
+     printf("Odd : %d\n");
+
+return 0;
+}

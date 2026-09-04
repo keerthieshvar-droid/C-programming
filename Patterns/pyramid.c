d415aa96d4049784114a6987055676392d6b5460
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-09-04 09:07:31 +0530

    Add files via upload

---
+++ Patterns/pyramid.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+       int n,s,i,j;
+       printf("nter a value:");
+       scanf("%d" , &n);
+
+       for( i = 0; i < n; i++)
+        {
+           for(s = 0; s < n-i-1; s++)
+                printf("  ");
+
+              for(j = 0; j < 2*i+1; j++)
+                   printf("* ");
+
+                 printf("\n");
+        }
+
+return 0;
+}

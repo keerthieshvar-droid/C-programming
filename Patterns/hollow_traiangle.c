68f246c6f678aab7e26af17eb1eee62b2c2715bc
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-09-05 10:22:55 +0530

    Add files via upload

---
+++ Patterns/hollow_traiangle.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+      int n,i,j,s;
+      printf("Enter a value: ");
+      scanf("%d" , &n);
+
+     for(i = 0; i < n; i++)
+     {
+       for(s = 0; s < n-i-1; s++)
+        {
+          printf("  ");
+        }
+
+          for(j = 0; j < 2*i+1; j++)
+            {
+                if(j == 0 ||j == 2*i ||i == n - 1)
+                    printf("* ");
+               else
+                    printf("  ");
+             }
+          printf("\n");
+      }
+
+  return 0;
+}
+

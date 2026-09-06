71c5592b778f866ef181cc0125555f5e19dff5ea
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-09-06 18:49:05 +0530

    Add files via upload

---
+++ Patterns/right_arrow.c
@@ -0,0 +1,24 @@
+ #include<stdio.h>
+int main()
+{
+    int n,i,j,s,l;
+    printf("Enter a value: ");
+    scanf("%d" , &n);
+
+    for(i = -n; i < n; i++)
+    {
+        if(i < 0)
+            l = -i;
+        else
+            l = i;
+      for(s = 0; s < n - l; s++)
+            printf("  ");
+      for(j = 0; j < l + 1; j++)
+             printf("* ");
+             printf("\n");
+     }
+
+ return 0;
+}
+
+

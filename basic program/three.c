e8fc6723fe5d7751ab0d8490ceaf45031d74e0da
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-09-07 20:33:14 +0530

    Add files via upload

---
+++ basic program/three.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{  
+           int a,b,c;
+           printf("Enter three numbers:");
+           scanf("%d %d %d", &a , &b , &c);
+
+           printf("Value of A = %d\n", a);
+           printf("Value of B = %d\n", b);
+           printf("Value of C = %d\n", c);
+
+      return 0;
+}

28ed026b412ba21706dac8fb77529e98964220a2
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-09-09 18:03:03 +0530

    Add files via upload

---
+++ basic program/typecas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+           float x = 21.35;
+           int y;
+
+           y = (int)x;
+ 
+           printf("Float value =%.2f\n" , x);
+           printf("Integer value = %d\n" , y);
+
+       return 0;
+} 

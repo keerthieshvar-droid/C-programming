760e62d2c00dfac609e83081910740546db60bf9
T R KEERTHIESHVAR <keerthieshvar@gmail.com>
2026-09-09 18:02:33 +0530

    Add files via upload

---
+++ basic program/token.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+          // Variable declaration
+          int num = 10;
+
+       /*  Displaying the value of the variables
+
+
+
+
+       */
+           printf("%d", num);
+
+        return 0;
+}
